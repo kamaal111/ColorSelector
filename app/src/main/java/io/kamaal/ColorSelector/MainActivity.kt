8164359ea566da5af14ab737b08334390e5350c7
Kamaal <kamaal@tranzer.com>
2021-05-28 16:18:48 +0200

    Designing preview box

--- app/src/main/java/io/kamaal/ColorSelector/MainActivity.kt
+++ app/src/main/java/io/kamaal/ColorSelector/MainActivity.kt
@@ -3,21 +3,42 @@ package io.kamaal.ColorSelector
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.kamaal.ColorSelector.ui.theme.ColorSelectorTheme
+import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColorSelectorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            ContentView()
+        }
+    }
+}
+
+@Composable
+fun ContentView() {
+    ColorSelectorTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            Column(modifier = Modifier.padding(horizontal = 40.dp, vertical = 40.dp)) {
+                PreviewBox(color = Color.Red)
+                Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    TitleValue(title = "Name", value = "Red", modifier = Modifier.padding(top = 8.dp))
                 }
             }
         }
@@ -25,14 +46,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TitleValue(title: String, value: String, modifier: Modifier = Modifier) {
+    Row(modifier = modifier.padding(vertical = 4.dp)) {
+        Text(text = title, fontWeight = FontWeight.SemiBold, fontSize = 17.sp)
+        Text(text = value, modifier = Modifier.padding(horizontal = 12.dp))
+    }
+}
+
+@Composable
+fun PreviewBox(color: Color, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .height(120.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12))
+            .background(color = color)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ColorSelectorTheme {
-        Greeting("Android")
-    }
+    ContentView()
 }
--- app/src/main/java/io/kamaal/ColorSelector/ui/theme/Color.kt
+++ app/src/main/java/io/kamaal/ColorSelector/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package io.kamaal.ColorSelector.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Marine200 = Color(0xA6E9C5)
+val Marine700 = Color(0x23AC6D)
+val Marine500 = Color(0x35D087)
+val Magenta200 = Color(0xD76CA4)
