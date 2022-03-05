b17a6f8a5b9b4df060f490aad65601fe0ae126d8
Kamaal <kamaal.f1@gmail.com>
2022-03-05 18:43:40 +0100

    Showing a app store button under qr code

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,9 +1,18 @@
 <template>
-  <img :src="getQRCodeSource()" />
+  <div id="content">
+    <img id="qr-code" :src="getQRCodeSource()" />
+    <button @click="appStoreButtonClick">
+      <img :src="appStoreSVG" />
+    </button>
+  </div>
 </template>
 
 <script>
 import { ref } from "vue";
+
+import AppStoreSVG from "../assets/app-store-black.svg";
+import QRCodeDarkImage from "../assets/qr-code-dark.jpg";
+import QRCodeLightImage from "../assets/qr-code-light.jpg";
 
 const darkModeMatch = matchMedia("(prefers-color-scheme: dark)");
 const isDarkMode = ref(darkModeMatch.matches);
@@ -16,24 +25,43 @@ matchMedia("(prefers-color-scheme: dark)").addEventListener(
 );
 
 const getQRCodeSource = () => {
-  const images = require.context("../assets", false, /\.jpg$/);
-  if (isDarkMode.value) {
-    return images("./qr-code-dark.jpg");
-  }
-  return images("./qr-code-light.jpg");
+  if (isDarkMode.value) return QRCodeDarkImage;
+  return QRCodeLightImage;
+};
+
+const appStoreButtonClick = () => {
+  console.log("yipiiii clicked");
 };
 
 export default {
+  data: () => ({
+    appStoreSVG: AppStoreSVG,
+  }),
   methods: {
     getQRCodeSource,
+    appStoreButtonClick,
   },
   name: "AppContent",
 };
 </script>
 
-<style>
+<style scoped>
+#content {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+
+#qr-code {
+  height: 200px;
+  width: 200px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 button {
-  font-weight: bold;
-  color: red;
+  border: 0;
+  user-select: none;
+  margin: 16px;
 }
 </style>
