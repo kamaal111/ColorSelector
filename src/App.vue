4717b9ee2d68312fcb04c8700edb5b7d2246faa7
Kamaal <kamaal.f1@gmail.com>
2022-03-05 22:17:16 +0100

    Adding a footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 	<AppHeader title="Color Selector" />
 	<AppContent />
+	<AppFooter />
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppContent from './components/AppContent.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
 	name: 'App',
 	components: {
 		AppHeader,
 		AppContent,
+		AppFooter,
 	},
 };
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="footer">
+		<p>Ⓒ 2022 Kamaal Farah</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AppFooter',
+};
+</script>
+
+<style scoped>
+#footer {
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+</style>
