71ba12a0911d6f85a3358454f9ee3ca5c29fa73f
Kamaal <kamaal.f1@gmail.com>
2022-03-05 12:46:32 +0100

    Initializing project as a vue project

--- app/src/main/java/io/kamaal/ColorSelector/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package io.kamaal.ColorSelector.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Marine200 = Color(0xA6E9C5)
-val Marine700 = Color(0x23AC6D)
-val Marine500 = Color(0x35D087)
-val Magenta200 = Color(0xD76CA4)
