a59fe356cc8e2cd2cbe6abd7dbe44e98d02e5e4a
Kamaal <kamaal.f1@gmail.com>
2022-03-12 18:46:58 +0100

    Showing a color preview in edit screen

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 
-import store from './store';
-
 const app = createApp(App);
 
-app.use(store);
 app.mount('#app');
--- src/screens/EditScreen.vue
+++ src/screens/EditScreen.vue
@@ -1,13 +1,37 @@
 <template>
-	<p>Edit</p>
+	<div id="color-preview" :style="previewStyle" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+
+const red = ref(152);
+const green = ref(129);
+const blue = ref(123);
+
+function previewStyle() {
+	return `background-color: rgba(${red.value},${green.value},${blue.value},1)`;
+}
 
 export default defineComponent({
 	name: 'EditScreen',
+	computed: {
+		previewStyle,
+	},
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use 'sass:map';
+
+@import '../styles/variables';
+
+$color-preview-size: 200px;
+
+#color-preview {
+	width: $color-preview-size;
+	height: $color-preview-size;
+	border-radius: map.get($sizes, 'medium');
+	margin: 0 auto;
+}
+</style>
