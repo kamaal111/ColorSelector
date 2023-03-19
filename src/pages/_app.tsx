5c29a11ea7d462affefec1b966f91eb6f1735d9c
Kamaal <kamaal.f1@gmail.com>
2023-03-19 15:07:31 +0100

    Copying values to the clipboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.scss";
+
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // @ts-ignore React typing is just straight broken
   return <Component {...pageProps} />;
 }
